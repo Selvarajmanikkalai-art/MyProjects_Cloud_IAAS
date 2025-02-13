81b1ca58064a7bbf113ed3a064a1970f83c3b95e
selvarajmanikkalai <smanikkalai@presidio.com>
2025-02-14 02:46:58 +0530

    provider_issue

--- Projects/ThreeTierWebServer-Terraform/Module_Network/provider.tf
+++ Projects/ThreeTierWebServer-Terraform/Module_Network/provider.tf
@@ -2,10 +2,12 @@
 // AWS project, It is a AWS Cloud architecture for web hosting:
 
 terraform {
-    required_providers {
-      aws = "hashicorp/aws"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
       version = "5.85.0"
     }
+  }
 }
 
 provider "aws" {
